4d9b3b21fd813df290466c0205d06757d5298b90
Juan Carlos Rojas <rojb.jc@gmail.com>
2021-04-01 11:35:26 -0600

    Initial Commit

---
+++ BACLogin_JCRB/BACLogin_JCRBApp.swift
@@ -0,0 +1,17 @@
+//
+//  BACLogin_JCRBApp.swift
+//  BACLogin_JCRB
+//
+//  Created by Juan Carlos  Rojas on 1/4/21.
+//
+
+import SwiftUI
+
+@main
+struct BACLogin_JCRBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BACLogin_JCRB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BACLogin_JCRB
+//
+//  Created by Juan Carlos  Rojas on 1/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
